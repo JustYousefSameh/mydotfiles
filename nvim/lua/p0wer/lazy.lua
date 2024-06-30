816a37c33a18588a743ba9807f65e324b734b3e4
JustYousefSameh <mr.yousefsameh@gmail.com>
2024-06-30 16:44:53 +0000

    updates

--- nvim/lua/p0wer/lazy.lua
+++ nvim/lua/p0wer/lazy.lua
@@ -40,4 +40,15 @@ require("lazy").setup({
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
 },
+{
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+},
 })
